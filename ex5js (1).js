e24279aa4a23c05c0cd69a27d8a06e4eaf8eaeb0
horizontals <100505378+horizontals@users.noreply.github.com>
2022-07-20 22:00:52 +0900

    Add files via upload

---
+++ ex5js (1).js
@@ -0,0 +1,15 @@
+﻿const makefriends=()=>{
+	alert("........");
+	const btn=document.getElementById("btn");
+	btn.innerHTML="We are FRIENDS!!!!";
+	const table=document.getElementById("table");
+	table.innerHTML="female";
+	table.style.color="red";
+}
+
+window.onload=setup=()=>{
+	const table=document.getElementById("table");
+	table.innerHTML="&nbsp;&nbsp;&ensp;male";
+}
+
+setup();
